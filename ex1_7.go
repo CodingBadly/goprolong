f5718c513824f53e57586f1680b9ad35d69a3ad8
CodingBadly <codingbadly@outlook.com>
2016-06-17 08:05:06 -0700

    ex1_7.go

---
+++ ex1_7.go
@@ -0,0 +1,33 @@
+// Ex 1.7:
+// The function call io.Copy(dst, src) reads from src adn writes to dst.
+// Use it instead of ioutil.ReadAll to copy the reponse body to os.Stdout
+// without requireing a buffer large enough to hold the entire stream.
+// Be sure to check the error result of io.Copy.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//b, err := ioutil.ReadAll(resp.Body)
+		//resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		//fmt.Printf("%s", b)
+	}
+}
